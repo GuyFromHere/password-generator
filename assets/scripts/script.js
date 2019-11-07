03fa2a6dd9c8cb7594b69c564860042127943916
GuyFromHere <47902660+GuyFromHere@users.noreply.github.com>
2019-11-06 16:46:51 -0800

    Javascript complete. Basic skeleton of html is up with no css.

---
+++ assets/scripts/script.js
@@ -0,0 +1,37 @@
+var criteriaArr = ["numbers", "special characters", "lower case", "upper case"];
+
+function getCriteria(type) {
+  var criteria = prompt("Use " + type + "? Y or N");
+  // validation here...
+  criteria = criteria.toLowerCase();
+  if (criteria === "y") return true;
+  else return false;
+}
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (+max - +min)) + +min;
+}
+
+function generatePassword(length) {
+  var criteria = "";
+  var password = "";
+  var chars = [
+    "0123456789",
+    "!@#$%^&*()_+~`|}{[]:;?><,./-=",
+    "abcdefghijklmnopqrstuvwxyz",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  ];
+
+  // Get password criteria based on user input
+  // Store in one long string
+  for (var i = 0; i < criteriaArr.length; i++) {
+    if (getCriteria(criteriaArr[i])) {
+      criteria += chars[i];
+    }
+  }
+  // Use random number generator to pick elements from criteria and add them to the new password
+  for (var x = 0; x <= length; x++) {
+    password += criteria[getRandom(0, criteria.length)];
+  }
+  document.getElementById("passwordText").innerHTML = password;
+}
